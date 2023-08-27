f54bf03396cf4be16cf5ef70ec16560e74233047
Joon <joonhyunwoo@naver.com>
2023-08-27 17:02:11 +0900

    ㅡ

---
+++ 노다가 중 copy/map_build.py
@@ -0,0 +1,13 @@
+from player import *
+from sprites import *
+
+def build_map(self, tilemap):
+    for i, row in enumerate(tilemap):
+        for j, column in enumerate(row):
+            Ground(self, j, i)
+            if column == "P":
+                self.Player = Player(self, j, i)
+            if column == "T":
+                Tree(self, j, i)
+            if column == "W":
+                Warp(self, j, i)
