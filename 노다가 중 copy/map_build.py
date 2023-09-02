fdd33cfad32a8eee9d8d658008739933d4bad465
Joon <joonhyunwoo@naver.com>
2023-09-02 14:12:23 +0900

    복사본

---
+++ 노다가 중 copy/map_build.py
@@ -0,0 +1,23 @@
+from player import *
+from sprites import *
+
+def build_map(self, tilemap):
+    for i, row in enumerate(tilemap):
+        for j, column in enumerate(row):
+            Wooden(self, j, i)
+            if column == "P":
+                self.Player = Player(self, j, i)
+            if column == "B":
+                Desk(self, j, i)
+            if column == "T":
+                Wall(self, j, i)
+            if column == "U":
+                Warp_Up(self, j, i)
+            if column == "D":
+                Warp_Down(self, j, i)
+            if column == "0":
+                Empty(self, j, i)
+            if column == "H":
+                Hallway(self, j, i)
+            if column == "C":
+                Couple(self, j, i)
